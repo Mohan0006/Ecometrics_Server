7331b38f64327d7ba82e82f44aa57a9f2691adff
mohansai <mohansaikrishna.m20@iiits.in>
2023-09-29 20:13:47 +0530

    cors

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ const getAir = require("./routes/airRoute");
 app.use(bodyParser.json());
 app.use((req, res, next) => {
   console.log('CORS middleware executed');
-  res.header('Access-Control-Allow-Origin', 'https://ecometrics-client.onrender.com/');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', '*');
   next();
 });
 
